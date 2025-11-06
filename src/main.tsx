b11a69f4cb68e64de700ba566ddebd90fde6dc1b
Macarron <marianacifuenteszu@gmail.com>
2025-11-06 14:02:23 -0500

    feature chat ia added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createChat } from "@n8n/chat";
+import "@n8n/chat/style.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createChat({
+  webhookUrl:
+    "https://macarron01.app.n8n.cloud/webhook/e8156249-8860-4140-9880-1b61133b60d4/chat",
+  mode: "window",
+  target: "#n8n-chat",
+  showWelcomeScreen: true,
+  initialMessages: ["¡Hola! 👋", "Soy BlessDBot. ¿Cómo puedo ayudarte hoy?"],
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <div id="n8n-chat"></div>
+  </StrictMode>
+);
