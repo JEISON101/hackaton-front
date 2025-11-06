a10d847a31afc6497d7c5e571d5fefa6fe721970
Macarron <marianacifuenteszu@gmail.com>
2025-11-06 12:08:03 -0500

    feature interface clients

--- src/pages/Clientes.tsx
+++ src/pages/Clientes.tsx
@@ -5,7 +5,6 @@ const Clientes = () => {
     <div className="bg-gray-50 text-black min-h-screen p-6">
       <h1 className="text-2xl text-black font-bold mb-6">Clientes</h1>
       <section className="mb-6">
-        <h2 className="text-xl font-semibold mb-4">Lista de Clientes</h2>
         <ClientTable />
       </section>
     </div>
--- src/pages/Usuarios.tsx
+++ src/pages/Usuarios.tsx
@@ -5,7 +5,6 @@ const Usuarios = () => {
     <div className="bg-gray-50 text-black min-h-screen p-6">
       <h1 className="text-2xl text-black font-bold mb-6">Usuarios</h1>
       <section className="mb-6">
-        <h2 className="text-xl font-semibold mb-4">Lista de Usuarios</h2>
         <UserTable />
       </section>
     </div>
