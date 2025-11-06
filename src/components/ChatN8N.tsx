a10b39e703c38fdb92bdfef4fd443b937e96901e
Jorge Porras <jorgeeporrassandoval@gmail.com>
2025-11-06 14:30:19 -0500

    Implementacion de chat

---
+++ src/components/ChatN8N.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { createChat } from "@n8n/chat";
+
+const ChatN8N: React.FC = () => {
+  useEffect(() => {
+    const chat = createChat({
+      webhookUrl:
+        "https://macarron01.app.n8n.cloud/webhook/e8156249-8860-4140-9880-1b61133b60d4/chat",
+      mode: "window",
+      target: "#n8n-chat",
+      showWelcomeScreen: true,
+      initialMessages: [
+        "¡Hola! 👋",
+        "Soy BlessDBot. ¿Cómo puedo ayudarte hoy?",
+      ],
+    });
+
+    return () => {
+      const container = document.getElementById("n8n-chat");
+      if (container) container.innerHTML = ""; // elimina el contenido
+    };
+  }, []);
+
+  return <div id="n8n-chat"></div>;
+};
+
+export default ChatN8N;
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,13 @@
-import { createChat } from "@n8n/chat";
 import "@n8n/chat/style.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createChat({
-  webhookUrl:
-    "https://macarron01.app.n8n.cloud/webhook/e8156249-8860-4140-9880-1b61133b60d4/chat",
-  mode: "window",
-  target: "#n8n-chat",
-  showWelcomeScreen: true,
-  initialMessages: ["¡Hola! 👋", "Soy BlessDBot. ¿Cómo puedo ayudarte hoy?"],
-});
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <div id="n8n-chat"></div>
   </StrictMode>
 );
